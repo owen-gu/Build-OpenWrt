dae596725239ae288f7e8358d0fefbd753fae13d
owen-gu <52304619+owen-gu@users.noreply.github.com>
2021-02-20 23:05:56 +0800

    update

--- customize.sh
+++ customize.sh
@@ -3,4 +3,4 @@ rm -rf openwrt/package/lean/vlmcsd
 rm -rf openwrt/package/lean/luci-app-vlmcsd
 rm -rf openwrt/package/lean/default-settings
 rm -rf openwrt/feeds/packages/net/sqm-scripts
-
+rm -rf openwrt/feeds/packages/net/luci-lib-docker
