5bb4c75da0bce9f6a3f1d848bfdc370a2b925a91
owen-gu <52304619+owen-gu@users.noreply.github.com>
2021-02-20 19:24:50 +0800

    update

--- customize.sh
+++ customize.sh
@@ -1,9 +1,6 @@
-rm -rf ./openwrt/package/lean/kcptun
-rm -rf ./openwrt/package/lean/vlmcsd
-rm -rf ./openwrt/package/lean/luci-app-vlmcsd
-rm -rf ./openwrt/package/lean/default-settings
-rm -rf ./openwrt/package/lean/shadowsocksr-libev
-rm -rf ./openwrt/feeds/packages/net/sqm-scripts
-cp -rf ./owen/* ./openwrt/package/lean/
-chmod 777 -R ./openwrt
+rm -rf openwrt/package/lean/kcptun
+rm -rf openwrt/package/lean/vlmcsd
+rm -rf openwrt/package/lean/luci-app-vlmcsd
+rm -rf openwrt/package/lean/default-settings
+rm -rf openwrt/feeds/packages/net/sqm-scripts
 
