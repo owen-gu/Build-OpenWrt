5ff3a22fceb70539c765db0e7512ca175701c751
owen-gu <52304619+owen-gu@users.noreply.github.com>
2021-02-24 20:58:02 +0800

    update

--- customize.sh
+++ customize.sh
@@ -1,7 +1,9 @@
 rm -rf openwrt/package/helloworld
 rm -rf openwrt/package/owen
 rm -rf openwrt/package/OpenAppFilter
-rm -rf openwrt/package/lean/kcptun
+
+
+
 rm -rf openwrt/package/lean/vlmcsd
 rm -rf openwrt/package/lean/luci-app-vlmcsd
 rm -rf openwrt/package/lean/default-settings
@@ -14,6 +16,8 @@ cd openwrt/package
 git clone --depth=1 https://github.com/fw876/helloworld
 git clone --depth=1 https://github.com/owen-gu/owen
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
+
+
 
 cd ..
 cd ..
