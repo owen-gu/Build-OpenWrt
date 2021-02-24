28000f37144d8d7be3aaaa29ca235ad086453983
owen-gu <52304619+owen-gu@users.noreply.github.com>
2021-02-24 08:47:13 +0800

    up

--- customize19.sh
+++ customize19.sh
@@ -1,6 +1,5 @@
 rm -rf ./openwrt/package/owen19
 rm -rf ./openwrt/package/openwrt-passwall
-rm -rf ./openwrt/package/lean/kcptun
 rm -rf ./openwrt/package/lean/vlmcsd
 rm -rf ./openwrt/package/lean/luci-app-vlmcsd
 rm -rf ./openwrt/package/default-settings
@@ -10,13 +9,12 @@ cd openwrt/package
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 git clone --depth=1 https://github.com/owen-gu/owen19
 
-cd ..
-
-rm -rf ./openwrt/package/openwrt-passwall/kcptun
+rm -rf ./openwrt-passwall/kcptun
 
 cd ..
 
-chmod 777 -R ./openwrt
+
+chmod 777 -R ../openwrt
 
 
  
