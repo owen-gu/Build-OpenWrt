484b0218105dd7f05585b7e13c12e885ea36de47
owen-gu <52304619+owen-gu@users.noreply.github.com>
2021-05-12 14:28:45 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,4 +1,4 @@
-#rm -rf openwrt/package/helloworld
+rm -rf openwrt/package/helloworld
 rm -rf openwrt/package/owen
 #rm -rf openwrt/package/OpenAppFilter
 
@@ -14,7 +14,7 @@ rm -rf openwrt/feeds/luci/applications/luci-app-nlbwmon
 cd openwrt/package
 # Add OpenAppFilter
 
-#git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld
 #git clone --depth=1 https://github.com/Mattraks/helloworld
 git clone --depth=1 https://github.com/owen-gu/owen
 #git clone --depth=1 https://github.com/destan19/OpenAppFilter
