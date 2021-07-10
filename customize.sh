fb6d703a010d36ff4c5eb0d4b471acad921c6996
owen-gu <52304619+owen-gu@users.noreply.github.com>
2021-07-10 13:30:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,6 +8,7 @@ rm -rf openwrt/package/lean/luci-app-vlmcsd
 rm -rf openwrt/package/lean/default-settings
 rm -rf openwrt/package/lean/luci-lib-docker
 rm -rf openwrt/feeds/luci/applications/luci-app-nlbwmon
+rm -rf openwrt/feeds/packages/net/kcptun
 
 cd openwrt/package
 # Add OpenAppFilter
