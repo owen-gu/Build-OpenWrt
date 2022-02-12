ec66e698342f12dc0d97a359db5c2677aafcdf1c
owen-gu <52304619+owen-gu@users.noreply.github.com>
2022-02-12 21:10:21 +0800

    up

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Build-OpenWrt
 
 云编译：  x86-64 和 EA8500 固件
-
-感谢 Lean 的 OpenWrt 库
--- customize.sh
+++ customize.sh
@@ -1,26 +1,26 @@
-rm -rf openwrt/package/helloworld
-rm -rf openwrt/package/owen
-#rm -rf openwrt/package/OpenAppFilter
+rm -rf ./openwrt/package/owen
 
 
-rm -rf openwrt/package/lean/vlmcsd
-rm -rf openwrt/package/lean/luci-app-vlmcsd
-rm -rf openwrt/package/lean/default-settings
-rm -rf openwrt/package/lean/luci-lib-docker
-rm -rf openwrt/feeds/luci/applications/luci-app-nlbwmon
-rm -rf openwrt/feeds/packages/net/kcptun
+rm -rf ./openwrt/feeds/packages/net/vlmcsd
+rm -rf ./openwrt/feeds/luci/applications/luci-app-vlmcsd
+rm -rf ./openwrt/package/emortal/default-settings
+rm -rf ./openwrt/feeds/luci/applications/luci-app-timecontrol
+
+
 
 cd openwrt/package
-# Add OpenAppFilter
-
-git clone --depth=1 https://github.com/fw876/helloworld
-#git clone --depth=1 https://github.com/Mattraks/helloworld
 git clone --depth=1 https://github.com/owen-gu/owen
-#git clone --depth=1 https://github.com/destan19/OpenAppFilter
-
 
 
 cd ..
 
 
 chmod 777 -R ../openwrt
+ 
+
+
+
+
+
+
+
