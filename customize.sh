3780f64ffa210ee3b05c99236d2e464961860c04
owen-gu <52304619+owen-gu@users.noreply.github.com>
2022-02-13 20:02:10 +0800

    up

--- customize.sh
+++ customize.sh
@@ -1,16 +1,17 @@
 rm -rf ./openwrt/package/owen
+rm -rf ./openwrt/package/helloworld
 
 
-rm -rf ./openwrt/feeds/packages/net/vlmcsd
-rm -rf ./openwrt/feeds/luci/applications/luci-app-vlmcsd
-rm -rf ./openwrt/package/emortal/default-settings
+rm -rf ./openwrt/package/lean/vlmcsd
+rm -rf ./openwrt/package/lean/luci-app-vlmcsd
+rm -rf ./openwrt/package/lean/default-settings
 rm -rf ./openwrt/feeds/luci/applications/luci-app-timecontrol
 
 
 
 cd openwrt/package
 git clone --depth=1 https://github.com/owen-gu/owen
-
+git clone --depth=1 https://github.com/fw876/helloworld
 
 cd ..
 
