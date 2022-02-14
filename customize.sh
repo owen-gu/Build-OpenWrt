6b3a8035c6167a5bacaf843fb30d458ffb83fccd
owen-gu <52304619+owen-gu@users.noreply.github.com>
2022-02-14 14:15:11 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,6 +5,7 @@ rm -rf ./openwrt/package/helloworld
 rm -rf ./openwrt/package/lean/vlmcsd
 rm -rf ./openwrt/package/lean/luci-app-vlmcsd
 rm -rf ./openwrt/package/lean/default-settings
+rm -rf ./openwrt/feeds/packages/net/kcptun
 
 
 
