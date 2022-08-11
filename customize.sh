11a9f058a3988434550f63762cc7de387bb37b19
owen-gu <52304619+owen-gu@users.noreply.github.com>
2022-08-11 12:02:09 +0800

    up

--- customize.sh
+++ customize.sh
@@ -1,18 +1,31 @@
 rm -rf ./openwrt/package/owen
-rm -rf ./openwrt/package/helloworld
 
 
-rm -rf ./openwrt/package/lean/vlmcsd
-rm -rf ./openwrt/package/lean/luci-app-vlmcsd
+
+rm -rf ./openwrt/feeds/packages/net/vlmcsd
+rm -rf ./openwrt/feeds/luci/applications/luci-app-vlmcsd
 rm -rf ./openwrt/package/lean/default-settings
+rm -rf ./openwrt/package/lean/autocore
 rm -rf ./openwrt/feeds/packages/net/kcptun
+rm -rf ./openwrt/feeds/packages/net/smartdns
 
-
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./openwrt/feeds/luci/collections/luci/Makefile
 
 
 cd openwrt/package
 git clone --depth=1 https://github.com/owen-gu/owen
-git clone --depth=1 https://github.com/fw876/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld owen/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall   owen/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2  owen/luci-app-passwall2
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall owen/luci-app-passwall
+git clone https://github.com/linkease/istore owen/istore
+git clone https://github.com/yaof2/luci-app-ikoolproxy owen/luci-app-ikoolproxy
+
+
+
+
+sed -i 's/+luci-lib-ipkg/  /g' ./owen/istore/luci/luci-app-store/Makefile
+
 
 cd ..
 
